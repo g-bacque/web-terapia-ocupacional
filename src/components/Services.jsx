fea985a8534dbaf8f02b0febcd8d40f7c97e72a3
g-bacque <gbbgithub@gmail.com>
2026-02-13 18:44:17 +0100

    Nuevos colores

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,9 +3,9 @@ import { FaChild, FaUserCheck, FaUsers } from "react-icons/fa";
 
 export default function Services() {
   const services = [
-    { title: "Evaluación funcional", description: "Enfoque personalizado basado en objetivos reales y cotidianos.", icon: <FaUserCheck size={40} color="#2563eb" /> },
-    { title: "Intervención individual", description: "Apoyo adaptado a cada persona según sus necesidades.", icon: <FaChild size={40} color="#2563eb" /> },
-    { title: "Acompañamiento familiar", description: "Guiamos a familias para mejorar la autonomía y bienestar.", icon: <FaUsers size={40} color="#2563eb" /> },
+    { title: "Evaluación funcional", description: "Enfoque personalizado basado en objetivos reales y cotidianos.", icon: <FaUserCheck size={40}  /> },
+    { title: "Intervención individual", description: "Apoyo adaptado a cada persona según sus necesidades.", icon: <FaChild size={40}  /> },
+    { title: "Acompañamiento familiar", description: "Guiamos a familias para mejorar la autonomía y bienestar.", icon: <FaUsers size={40} /> },
   ];
 
   return (
